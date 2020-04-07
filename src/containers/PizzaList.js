080b1eb7061267393c4afb41bdac093bab477927
mdiaz26 <mike.diaz006@gmail.com>
2020-04-07 10:34:28 -0400

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
-
   render() {
     return (
       <table className="table table-striped">
@@ -15,7 +14,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.props.pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} handleClick={this.props.handleClick}/>)
           }
         </tbody>
       </table>
