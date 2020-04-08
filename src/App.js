861efe26309805aadfaf4063ad425cd21b478d8c
github username <github email address>
2020-04-07 21:54:36 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component, Fragment } from 'react';
-import Header from './components/Header'
-import PizzaForm from './components/PizzaForm'
-import PizzaList from './containers/PizzaList'
+import Header from './components/Header';
+import PizzaForm from './components/PizzaForm';
+import PizzaList from './containers/PizzaList';
+const pizzaURL = 'http://localhost:4000/pizzas';
+const initialState = {
+  id: -1,
+  topping: '',
+  size: '',
+  vegetarian: false,
+};
 class App extends Component {
+  state = initialState;
+
+  postFetch = () => {
+    let data = this.state;
+    fetch(`${pizzaURL}/${this.state.id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        accpets: 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(() => this.setState(initialState));
+  };
+
+  onEdit = (obj) => {
+    let pizza = obj;
+    delete pizza.data;
+    this.setState(pizza);
+  };
+  onSubmit = (event) => {
+    if (event.target.name === 'vegetarian') {
+      this.setState({ [event.target.name]: !this.state.vegetarian });
+    } else {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+  };
+
   render() {
     return (
       <Fragment>
-        <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <Header />
+        <PizzaForm
+          pizza={this.state}
+          postFetch={this.postFetch}
+          onSubmit={this.onSubmit}
+        />
+        <PizzaList onEdit={this.onEdit} pizza={this.state} />
       </Fragment>
     );
   }
